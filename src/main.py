990feb9f82b4cde5f10f9429cd1d52e6e3c57d58
tct123 <42028373+tct123@users.noreply.github.com>
2024-12-12 22:35:43 +0100

    .

--- src/main.py
+++ src/main.py
@@ -2,7 +2,8 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    audio = ft.Audio(src="")
+    audio = ft.Audio(src="assets/we-wish-you-a-merry-christmas.wav",autoplay=True)
+    page.overlay.append(audio)
     counter = ft.Text("Merry Christmas", size=50, data=0)
 
     def increment_click(e):
@@ -10,13 +11,22 @@ def main(page: ft.Page):
         counter.value = str(counter.data)
         counter.update()
 
-    page.floating_action_button = ft.FloatingActionButton(
-        icon=ft.Icons.ADD, on_click=increment_click
-    )
+    # page.floating_action_button = ft.FloatingActionButton(
+    #    icon=ft.Icons.ADD, on_click=increment_click
+    # )
     page.add(
         ft.SafeArea(
             ft.Container(
                 counter,
+                alignment=ft.alignment.center,
+            ),
+            expand=True,
+        )
+    )
+    page.add(
+        ft.SafeArea(
+            ft.Container(
+                ft.,
                 alignment=ft.alignment.center,
             ),
             expand=True,
